a7670bbbb1c0b98d577d506a72134a89c6fbcdfb
BoyTheTall <lungelowmhlanga@gmail.com>
2025-09-11 21:55:11 +0200

    this will be the backend of the image convertor thing. every needed fucntion will be here. Might make the UI reside in the same file for simplicity

---
+++ convert.py
@@ -0,0 +1,48 @@
+import img2pdf as im
+import PIL
+import os
+
+def create_folder(folder_dir):
+    try:
+        os.mkdir(folder_dir)
+    except FileExistsError:
+        print("Folder already exists")
+    except PermissionError:
+        print("Permission denied. OS said no, try another directory for the folder")
+    except Exception as e:
+        print(f"an exception occured: {e}")
+
+def convert_single_image_to_pdf(image_dir, output_folder):
+    create_folder(output_folder)
+    image = PIL.Image.open(image_dir)
+    pdf_bytes = im.convert(image.filename)
+    file = open(output_folder + f"/{image.filename.split('.')[0]}.pdf", 'wb')
+    file.write(pdf_bytes)
+    image.close()
+    file.close()
+    print("converted image successfully")
+
+#this grabs the images in a folder and sorts them before conversion
+def convert_multiple_images(images_dir_folder, output_folder):
+    images = []
+    for fname in os.listdir(images_dir_folder):
+        if not fname.endswith((".jpg", ".png", ".jpeg")):
+            continue
+        
+        path = os.path.join(images_dir_folder, fname)
+        if(os.path.isdir(path)):
+            continue
+        images.append(path)
+    images.sort()
+    create_folder(output_folder)
+    with open(f"{output_folder}/{create_output_file_name(images_dir_folder)}.pdf", "wb") as f:
+        f.write(im.convert(images))
+    
+def create_output_file_name(images_dir_folder):
+    if '/' in images_dir_folder:
+        return images_dir_folder.split('/')[-1]
+    else:
+        return images_dir_folder.split("\\")[-1]
+    
+
+convert_multiple_images("snek", "sponge")
